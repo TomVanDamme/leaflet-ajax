2431f6d12e82f8f17cd07e92c538007816e501be
Tom Van Damme <tom.vandamme@hogent.be>
2024-04-12 08:31:06 +0200

    Base Version

---
+++ js/myscript.js
@@ -0,0 +1,12 @@
+let map;
+
+function setupMap() {
+    // Initialize Leaflet map
+    map = L.map("map").setView([51.0538, 3.725], 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+}
+
+setupMap();
