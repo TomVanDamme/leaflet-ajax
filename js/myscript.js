6527ab062bc5305cf41ccdc48e8574c2d60673f2
Tom Van Damme <tom.vandamme@hogent.be>
2024-04-15 14:27:34 +0200

    wfs gemeenten

--- js/myscript.js
+++ js/myscript.js
@@ -9,4 +9,36 @@ function setupMap() {
     }).addTo(map);
 }
 
+function setupHandlers() {
+    document.querySelector("#btnToonGemeenten").addEventListener("click", toonGemeenten);
+}
+
+async function toonGemeenten() {
+    const box = map.getBounds();
+
+    const base_url = "https://geo.api.vlaanderen.be/VRBG/wfs";
+    const params = {
+        request: "getFeature",
+        version: "1.3.0",
+        service: "WFS",
+        outputFormat: "application/json",
+        typeName: "VRBG:Refgem",
+        BBOX: `${box.getWest()},${box.getSouth()},${box.getEast()},${box.getNorth()},EPSG:4326`,
+        srsName: "EPSG:4326",
+    };
+
+    const url = new URL(base_url);
+    url.search = new URLSearchParams(params);
+
+    console.log(url);
+    const response = await fetch(url);
+
+    const json = await response.json();
+
+    L.geoJSON(json).addTo(map);
+
+    console.log(json);
+}
+
 setupMap();
+setupHandlers();
