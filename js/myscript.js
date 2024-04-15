df81c474b6d8116e686ada8757a5f0ec9b8c6c03
Tom Van Damme <tom.vandamme@hogent.be>
2024-04-15 16:16:06 +0200

    wms + GetFeatureInfo

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,8 @@
 let map;
 
+const WMS_URL = "https://geo.api.vlaanderen.be/GRB/wms";
+const WMS_LAYER = "GRB_ADP";
+
 function setupMap() {
     // Initialize Leaflet map
     map = L.map("map").setView([51.0538, 3.725], 13);
@@ -7,36 +10,49 @@ function setupMap() {
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
+
+    L.tileLayer
+        .wms(WMS_URL, {
+            layers: WMS_LAYER,
+            version: "1.3.0",
+            transparent: true,
+            format: "image/png",
+        })
+        .addTo(map);
 }
 
 function setupHandlers() {
-    document.querySelector("#btnToonGemeenten").addEventListener("click", toonGemeenten);
+    map.on("click", getFeatureInfo);
 }
 
-async function toonGemeenten() {
-    const box = map.getBounds();
+async function getFeatureInfo(evt) {
+    const lon = evt.latlng.lng;
+    const lat = evt.latlng.lat;
+    const bbox = `${lat},${lon},${lat + 0.00001},${lon + 0.00001}`;
 
-    const base_url = "https://geo.api.vlaanderen.be/VRBG/wfs";
     const params = {
-        request: "getFeature",
+        service: "WMS",
         version: "1.3.0",
-        service: "WFS",
-        outputFormat: "application/json",
-        typeName: "VRBG:Refgem",
-        BBOX: `${box.getWest()},${box.getSouth()},${box.getEast()},${box.getNorth()},EPSG:4326`,
-        srsName: "EPSG:4326",
+        request: "GetFeatureInfo",
+        crs: "EPSG:4326",
+        bbox: bbox,
+        i: 116,
+        width: 256,
+        j: 105,
+        height: 256,
+        layers: WMS_LAYER,
+        query_layers: WMS_LAYER,
+        info_format: "application/json",
     };
 
-    const url = new URL(base_url);
+    const url = new URL(WMS_URL);
     url.search = new URLSearchParams(params);
 
     console.log(url);
+
     const response = await fetch(url);
 
-    const json = await response.json();
-
-    L.geoJSON(json).addTo(map);
-
+    const json = await response.text();
     console.log(json);
 }
 
